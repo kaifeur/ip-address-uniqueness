a6dd5cd56e1a63559d022a68031c8229ec217eaa
Nikolai Rubtsov <nikolay1337@icloud.com>
2022-03-13 14:35:47 +0300

    Add IntArrayUBitSetTest#shouldUnsetBitValue test

--- src/test/java/nyu/rubtsov/ip/address/uniquer/bitset/IntArrayUBitSetTest.java
+++ src/test/java/nyu/rubtsov/ip/address/uniquer/bitset/IntArrayUBitSetTest.java
@@ -4,7 +4,9 @@ import java.util.Random;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
+
+import static java.lang.Integer.toUnsignedLong;
 
 class IntArrayUBitSetTest {
 
@@ -22,24 +24,43 @@ class IntArrayUBitSetTest {
         bitSet = null;
     }
 
-    @Test
+    @RepeatedTest(100)
     void shouldSetBitValue() {
         int uIntIndex = random.nextInt();
         boolean previousValue = bitSet.set(uIntIndex);
         Assertions.assertTrue(
                 previousValue,
-                "Bit by index " + ((long) uIntIndex) + " is already set to 1"
+                "Bit by index " + toUnsignedLong(uIntIndex) + " is already set to 1"
         );
 
         boolean storedValue = bitSet.getValue(uIntIndex);
         Assertions.assertTrue(
                 storedValue,
-                "Bit value by index " + ((long) uIntIndex) + " was not changed"
+                "Bit value by index " + toUnsignedLong(uIntIndex) + " was not changed"
         );
     }
 
-    @Test
-    void unset() {
+    @RepeatedTest(100)
+    void shouldUnsetBitValue() {
+        int uIntIndex = random.nextInt();
+
+        boolean wasChanged = bitSet.set(uIntIndex);
+        Assertions.assertTrue(
+                wasChanged,
+                "Bit by index " + toUnsignedLong(uIntIndex) + " is already set to 1"
+        );
+
+        wasChanged = bitSet.unset(uIntIndex);
+        Assertions.assertTrue(
+                wasChanged,
+                "Bit by index " + toUnsignedLong(uIntIndex) + " was not changed by unset"
+        );
+
+        boolean storedValue = bitSet.getValue(uIntIndex);
+        Assertions.assertFalse(
+                storedValue,
+                "Bit value by index " + toUnsignedLong(uIntIndex) + " has unexpected value"
+        );
     }
 
     @Test
