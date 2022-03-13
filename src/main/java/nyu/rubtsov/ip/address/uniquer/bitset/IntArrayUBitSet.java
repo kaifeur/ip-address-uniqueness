cfa9ea16d5e1aa568ec4e64d15588f055df2341b
Nikolai Rubtsov <nikolay1337@icloud.com>
2022-03-13 14:21:25 +0300

    Fixes and logging in IntArrayUBitSet

--- src/main/java/nyu/rubtsov/ip/address/uniquer/bitset/IntArrayUBitSet.java
+++ src/main/java/nyu/rubtsov/ip/address/uniquer/bitset/IntArrayUBitSet.java
@@ -1,11 +1,20 @@
 package nyu.rubtsov.ip.address.uniquer.bitset;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static java.lang.Integer.toUnsignedLong;
+import static nyu.rubtsov.ip.address.uniquer.utils.BinaryStringUtils.toBinaryString;
+
 public class IntArrayUBitSet implements UIntBitSet {
 
-    public static final int MAX_BIT_COUNT = 1 << 31; // 2^32
+    private static final Logger logger = LoggerFactory.getLogger(IntArrayUBitSet.class);
 
-    private static final int UINT_BITS = 1 << 4; // 2^5
-    private static final int INT_ARRAY_SIZE = MAX_BIT_COUNT >>> UINT_BITS; // 2^32 bits at all, each int can handle 2^5 bits
+    public static final int MAX_BIT_COUNT = -1; // 2^32
+
+    private static final int CONTAINER_INDEX_MASK = (MAX_BIT_COUNT >>> 5) << 5; // left 27 bits in an unsigned int
+    private static final int BIT_INDEX_MASK = (1 << 5) - 1; // right 5 bits in an unsigned int
+    private static final int INT_ARRAY_SIZE = MAX_BIT_COUNT >>> 5; // 2^32 bits at all, each int can handle 2^5 bits
 
     private final int[] bits = new int[INT_ARRAY_SIZE];
 
@@ -14,11 +23,13 @@ public class IntArrayUBitSet implements UIntBitSet {
 
     @Override
     public synchronized boolean set(int uIntIndex) {
+        logger.debug("Setting bit by index {}", Integer.toUnsignedLong(uIntIndex));
         return setBitTo(uIntIndex, true);
     }
 
     @Override
     public synchronized boolean unset(int uIntIndex) {
+        logger.debug("Unsetting bit by index {}", Integer.toUnsignedLong(uIntIndex));
         return setBitTo(uIntIndex, false);
     }
 
@@ -30,25 +41,42 @@ public class IntArrayUBitSet implements UIntBitSet {
      * @return true if bit's value was changed, false otherwise
      */
     private boolean setBitTo(int uIntIndex, boolean value) {
+        Long uIntIndexAsLong;
+        if (logger.isTraceEnabled()) {
+            uIntIndexAsLong = toUnsignedLong(uIntIndex);
+        } else uIntIndexAsLong = null;
+
         int intContainerIndex = getIntContainerIndex(uIntIndex);
+        logger.trace("Container index for bit {} is {}", uIntIndexAsLong, intContainerIndex);
+
         int intContainer = getIntContainer(intContainerIndex);
+        logger.trace("Container value for bit {} is {}", uIntIndexAsLong, toBinaryString(intContainer));
 
         int bitIndex = getBitIndex(uIntIndex);
-        int bitValue = getBitValue(intContainer, bitIndex);
+        logger.trace("Bit index for bit {} is {}", uIntIndexAsLong, bitIndex);
 
-        int expectedValue = value ? 1 : 0;
+        boolean bitValue = getBitValue(intContainer, bitIndex);
+        logger.trace("Bit value for bit {} is {}", uIntIndexAsLong, bitValue ? 1 : 0);
 
-        if (bitValue != expectedValue) {
+        if (bitValue != value) {
+            logger.trace("Bit {} value ({}) != new value ({})",
+                    uIntIndexAsLong, bitValue ? 1 : 0, value ? 1 : 0);
+
+            int bitIndexAsInt = 1 << bitIndex;
+            logger.trace("Bit {} index as int is {}", uIntIndexAsLong, bitIndexAsInt);
+
             if (value) {
-                intContainer = intContainer | (1 << bitIndex);
+                intContainer = intContainer | bitIndexAsInt;
             } else {
-                intContainer = intContainer & (1 << bitIndex);
+                intContainer = intContainer & ~bitIndexAsInt;
             }
+            logger.trace("New container value is {} with bit by index {}", toBinaryString(intContainer), bitIndex);
 
             bits[intContainerIndex] = intContainer;
 
             return true;
         } else {
+            logger.trace("Bit {} is already set to {}", uIntIndexAsLong, bitValue);
             return false;
         }
     }
@@ -58,14 +86,14 @@ public class IntArrayUBitSet implements UIntBitSet {
     }
 
     private int getIntContainerIndex(int uIntIndex) {
-        return uIntIndex >>> UINT_BITS;
+        return uIntIndex & CONTAINER_INDEX_MASK;
     }
 
     private static int getBitIndex(int uIntIndex) {
-        return uIntIndex & ((UINT_BITS << 1) - 1);
+        return uIntIndex & BIT_INDEX_MASK;
     }
 
-    private static int getBitValue(int intContainer, int bitIndex) {
-        return intContainer & (1 << bitIndex);
+    private static boolean getBitValue(int intContainer, int bitIndex) {
+        return (intContainer & (1 << bitIndex)) != 0;
     }
 }
